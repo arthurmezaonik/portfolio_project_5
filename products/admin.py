17ab048b446cc79fccd5ff93dd2ee4d3f689a737
arthurmezaonik <arthur.mezaonik@gmail.com>
2022-03-14 19:52:54 +0000

    Create products models, url and views

--- products/admin.py
+++ products/admin.py
@@ -1,3 +1,55 @@
 from django.contrib import admin
+from .models import Product, Collection, Review
 
 # Register your models here.
+
+
+class ProductAdmin(admin.ModelAdmin):
+    list_display = (
+        'sku',
+        'title',
+        'collections',
+        'price',
+        'calories',
+        'image',
+    )
+
+    ordering = ('sku',)
+
+
+class CollectionAdmin(admin.ModelAdmin):
+    list_display = (
+        'friendly_name',
+        'name',
+    )
+
+
+class ReviewAdmin(admin.ModelAdmin):
+    list_display = (
+        'name',
+        'body',
+        'post',
+        'created_on',
+        'approved'
+    )
+
+    list_filter = (
+        'approved',
+        'created_on'
+    )
+
+    search_fields = (
+        'name',
+        'email',
+        'body'
+    )
+
+    actions = ['approve_comments']
+
+    def approve_comments(self, request, queryset):
+        queryset.update(approved=True)
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Collection, CollectionAdmin)
+admin.site.register(Review, ReviewAdmin)
--- products/models.py
+++ products/models.py
@@ -1,3 +1,48 @@
 from django.db import models
 
-# Create your models here.
+
+class Collection(models.Model):
+
+    name = models.CharField(max_length=254)
+    friendly_name = models.CharField(max_length=254, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Product(models.Model):
+    collections = models.ForeignKey(
+        'Collection',
+        null=True,
+        blank=True,
+        on_delete=models.SET_NULL
+    )
+    sku = models.CharField(max_length=254, null=True, blank=True)
+    title = models.CharField(max_length=254)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    calories = models.DecimalField(max_digits=6, decimal_places=2)
+    image_url = models.URLField(max_length=1024, null=True, blank=True)
+    image = models.ImageField(null=True, blank=True)
+
+    def __str__(self):
+        return self.title
+
+
+class Review(models.Model):
+    post = models.ForeignKey('Product', on_delete=models.CASCADE,
+                             related_name="reviews")
+    name = models.CharField(max_length=80)
+    email = models.EmailField()
+    body = models.TextField()
+    created_on = models.DateTimeField(auto_now_add=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ["created_on"]
+
+    def __str__(self):
+        return f"Comment {self.body} by {self.name}"

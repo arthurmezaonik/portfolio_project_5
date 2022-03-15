fc0c738f8499ac77ffc9b52733274751391ae956
arthurmezaonik <arthur.mezaonik@gmail.com>
2022-03-15 21:29:56 +0000

    Create a view and a url to handle specific products

--- products/urls.py
+++ products/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.all_products, name='products'),
+    path('<int:product_id>', views.specific_product, name='specific_product'),
 ]
--- products/views.py
+++ products/views.py
@@ -1,11 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from .models import Product, Collection
 
 # Create your views here.
 def all_products(request):
-    """
-    A view to render a list with all the products
-    """
+    """A view to render a list with all the products"""
 
     products = Product.objects.all()
     collection = None
@@ -23,3 +21,14 @@ def all_products(request):
 
     return render(request, 'products/products.html', context)
 
+
+def specific_product(request, product_id):
+    """A view to render a specific product"""
+
+    product = get_object_or_404(Product, pk=product_id)
+
+    context = {
+        'product': product,
+    }
+
+    return render(request, 'products/specific_product.html', context)

f620e2707323400db3f7be08bb901c4eeb83d503
arthurmezaonik <arthur.mezaonik@gmail.com>
2022-03-15 20:04:36 +0000

    Add the hability to display only the products that match with a specific collection

--- products/views.py
+++ products/views.py
@@ -1,16 +1,24 @@
 from django.shortcuts import render
-from .models import Product
-
+from .models import Product, Collection
 
 # Create your views here.
 def all_products(request):
     """
     A view to render a list with all the products
     """
+
     products = Product.objects.all()
+    collection = None
+
+    if request.GET:
+        if "collection" in request.GET:
+            requested_collection = request.GET["collection"]
+            products = products.filter(collections__name=requested_collection)
+            collection = Collection.objects.filter(name=requested_collection)[0]
 
     context = {
         'products': products,
+        'collection': collection,
     }
 
     return render(request, 'products/products.html', context)

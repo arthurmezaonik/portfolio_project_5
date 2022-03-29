3f237714f98954541fedf9745165a87e3e4b28ad
arthurmezaonik <arthur.mezaonik@gmail.com>
2022-03-29 09:21:08 +0000

    Create bag app, urls and templates

--- fit_at_home/urls.py
+++ fit_at_home/urls.py
@@ -10,5 +10,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('products/', include('products.urls')),
     path('blog/', include('blog.urls')),
-    path('search/', include('search.urls'))
+    path('search/', include('search.urls')),
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)

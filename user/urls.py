73f2d0cc77acad40e4204d9a123148004146f396
arthurmezaonik <arthur.mezaonik@gmail.com>
2022-03-30 13:32:45 +0000

    Add user area template and view, update webhoo

--- user/urls.py
+++ user/urls.py
@@ -2,5 +2,10 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.user_area, name='user_area')
+    path('', views.user_area, name='user_area'),
+    path(
+        'order_history/<order_number>',
+        views.order_history,
+        name='order_history'
+    ),
 ]

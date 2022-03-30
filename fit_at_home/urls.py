03d995e0cde29bad2f0e7ad1e04cd1cb685adacd
arthurmezaonik <arthur.mezaonik@gmail.com>
2022-03-30 23:55:57 +0000

    Deployment commit part 6

--- fit_at_home/urls.py
+++ fit_at_home/urls.py
@@ -18,7 +18,7 @@ urlpatterns = [
     path('newsletter/', include('newsletter.urls')),
 ] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
-if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 
 handler404 = 'home.views.error_404'
